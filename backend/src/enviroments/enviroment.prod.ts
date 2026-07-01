64fa54d78251a13ab0c281f8ebb5bf95961bfa21
Lutendo Matshidze <Lupreshie@gmail.com>
2026-07-01 02:44:18 +0200

    fix: resolve typo in production jwt environment variable

--- backend/src/enviroments/enviroment.prod.ts
+++ backend/src/enviroments/enviroment.prod.ts
@@ -2,6 +2,6 @@ import { Enviroment } from "./enviroment";
 
 export const ProdEnviroment = (): Enviroment => ({
     db_uri: process.env.PROD_DB_URI || "",
-    jwt_access_secret_key: process.env.DEV_JWT_ACCESS_TOKEN_SECRET_KEY!
+    jwt_access_secret_key: process.env.PROD_JWT_ACCESS_TOKEN_SECRET_KEY!
 
 });
